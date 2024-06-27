6cfe0b17a24740544b3b5a247f0f471942212e57
Otto Olivarez <2061768640101@ingenieria.usac.edu.gt>
2024-06-27 00:25:51 -0600

    Reseña, formulario para nueva reseña, listado de reseñas

--- frontend/frontend-views/src/UserData/components/UserReview.js
+++ frontend/frontend-views/src/UserData/components/UserReview.js
@@ -1,12 +1,26 @@
 import React,{useState,useEffect} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './MyNavBar';
+//import { set } from 'mongoose';
 
 const UserReview = () => {
     const [books, setBooks] = useState([]);
-    const [expandedLibro, setExpandedLibro] = useState(null);
-    const [showModal, setShowModal] = useState(false);
+    const [reviews, setReviews] = useState([]);
+    const [showModal, setShowModal] = useState(false); //Para mostrar el fromulario añadir reseña
+    const [showModalReviews, setShowModalReviews] = useState(false); //Para mostrar las reseñas
+    const [singleReview, setSingleReview] = useState({
+      IdUser: '',
+      IdBook: '',
+      review: '',
+      rating: 0,
+    });
 
+    //Guardo el id del usuario en el estado singleReview
+    useEffect(() => {
+      setSingleReview(prevstate => ({...prevstate, IdUser: localStorage.getItem('uid')}));
+    }, []);
+
+    // Listado de libros 
     useEffect(() => {
         const fetchLibros = async () => {
           try {
@@ -25,10 +39,29 @@ const UserReview = () => {
         fetchLibros();
       }, []);
 
-      const toggleAccordion = (libroId) => {
-        setExpandedLibro(expandedLibro === libroId ? null : libroId);
-      };
+      // Listado de reseñas para un libro específico
+      const handleViewReviews = async (id) => {
+        try {
+          const response = await fetch(`http://localhost:5000/api/reviews/:${id}`);
+          if (!response.ok) {
+           alert('Error al obtener reseñas');
+           throw new Error('Error al obtener reseñas');
+          }
+          //setShowModalReviews(true);
+          const data = await response.json();
+          setReviews(data.reviews);
+        } catch (error) {
+          alert('Error al obtener reseñas');
+          console.error('Error al obtener las reseñas', error);
+        }
+      }
 
+      // Nueva Reseña
+
+      //  Para guardar los datos del fromulario
+     /* const handleAddReview = (e,field) => {
+        setSingleReview({ ...singleReview, [field]: e.target.value });
+      }*/
 
   return (
     <div>
@@ -37,75 +70,77 @@ const UserReview = () => {
         <h2 className="my-4">Reseñas</h2>
         {/* Agregar buscar libros */}
         {books.map((libro, index) => {
-          const isExpanded = expandedLibro === libro._id;
-          //const isEditable = editableLibro === libro._id;
-          const collapseId = `collapse${index + 1}`;
-
+          
           return (
             <div key={libro._id} className="card mb-3">
-              <div className="card-body d-flex justify-content-between align-items-center">
-                <div>
-                  <h5 className="card-title">{libro.Titulo}</h5>
-                  <p className="card-text">Autor: {libro.autor}</p>
-                  <p className="card-text">Stock: {libro.cantidad_stock}</p>
+              <div class="row g-0">
+                <div class="col-md-2">
+                  {libro.imagen_url && (
+                    <img
+                      class="img-fluid rounded-start"
+                      src={libro.imagen_url}
+                      alt="Portada del libro"
+                      style={{ width: '100px', margin: '10px' }}
+                    />
+                  )}
                 </div>
-                <div className="d-flex align-items-center">
-                  <button
-                    className="btn btn-primary me-2"
-                    onClick={() => toggleAccordion(libro._id)}
-                    aria-expanded={isExpanded}
-                    aria-controls={collapseId}
-                  >
-                    +
-                  </button>
-                  <button
-                    className="btn btn btn-warning"
-                    onClick={() => {setShowModal(true);
-                       // setAddToCart({userId: '', book: libro._id, quantity: 0})
-                    }}
-                  >
-                    Añadir al carrito
-                  </button>
-                </div>
-              </div>
-              <div id={collapseId} className={`collapse ${isExpanded ? 'show' : ''}`}>
+                <div class="col-md-8">
 
-                  <div className="card-body">
-                    <p>Descripción: {libro.descripcion}</p>
-                    <p>Género: {libro.genero}</p>
-                    <p>Fecha de publicación: {new Date(libro.fecha_publicacion).toLocaleDateString()}</p>
-                    <p>Disponibilidad: {libro.disponibilidad ? 'Disponible' : 'No disponible'}</p>
-                    <p>Cantidad en stock: {libro.cantidad_stock}</p>
-                    <p>Puntuación promedio: {libro.puntuacion_promedio}</p>
-                    <p>Precio: ${libro.precio}</p>
-                    {libro.imagen_url && (
-                      <img
-                        src={libro.imagen_url}
-                        alt="Portada del libro"
-                        style={{ width: '100px', marginTop: '10px' }}
-                      />
-                    )}
+                  <div className="card-body d-flex justify-content-between align-items-center">
+                    <div>
+                      <h5 className="card-title">{libro.Titulo}</h5>
+                      <p className="card-text">Autor: {libro.autor}</p>
+                      <p className="card-text">Descripción: {libro.descripcion}</p>
+                    </div>
+                    <div className="d-flex align-items-center">
+                      <button
+                        className="btn btn-primary me-2"
+                        onClick={() => {
+                          setShowModalReviews(true);
+                        }}
+                      >
+                        Ver Reseñas
+                      </button>
+                      <button
+                        className="btn btn-success"
+                        onClick={() => {handleViewReviews(libro._id);setShowModal(true);}}
+                      >
+                        Añadir reseña
+                      </button>
+                    </div>
                   </div>
+
+                </div>
+                
               </div>
             </div>
           );
         })}
-
+        {/*Formularo para agregar reseña*/}
         {showModal && (
           <div className="modal show d-block" tabIndex="-1">
             <div className="modal-dialog">
               <div className="modal-content">
                 <div className="modal-header">
-                  <h5 className="modal-title">Agregar al carrito</h5>
+                  <h5 className="modal-title">Agregar reseña</h5>
                   <button type="button" className="btn-close" onClick={() => setShowModal(false)}></button>
                 </div>
                 <div className="modal-body">
                   <div className="mb-3">
-                    <label className="form-label">Cantidad</label>
+                    <label className="form-label">Clasificación</label>
                     <input
                       type="number"
                       className="form-control"
-                      //value={addToCart.quantity}
+                      value={singleReview.rating}
+                      //onChange={(e) => handleAddBook(e, 'quantity')}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Reseña</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      value={singleReview.review}
                       //onChange={(e) => handleAddBook(e, 'quantity')}
                     />
                   </div>
@@ -123,6 +158,40 @@ const UserReview = () => {
               </div>
             </div>
           </div>
+        )}
+
+        {/*Reseñas*/}
+        {showModalReviews && (
+          <div className="modal show d-block" tabIndex="-1">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Ver reseñas</h5>
+                <button type="button" className="btn-close" onClick={() => setShowModalReviews(false)}></button>
+              </div>
+              <div className="modal-body">
+                {reviews.map((review, index) => {
+                  return (
+                    <div key={index} className="card mb-3">
+                      <div className="card-body">
+                        <p>Reseña: {review.review}</p>
+                        <p>Clasificación: {review.rating}</p>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" onClick={() => setShowModalReviews(false)}>
+                  Cancelar
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
         )}
       </div>
     </div>
